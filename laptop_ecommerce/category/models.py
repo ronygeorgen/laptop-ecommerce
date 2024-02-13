c713e803227691f8157c94ad09d821d0a6634e28
Rony George <ronygeorgeofficial65@gmail.com>
2024-02-13 09:32:07 +0530

    third commit

--- laptop_ecommerce/category/models.py
+++ laptop_ecommerce/category/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.utils.text import slugify
 # Create your models here.
 class Category(models.Model):
-    category_name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=100,unique=True, blank=True)
-    description = models.CharField(max_length=255, blank = True)
-    cat_image = models.ImageField(upload_to='photos/categories/',blank=True)
+    category_name   = models.CharField(max_length=50, unique=True)
+    slug            = models.SlugField(max_length=100,unique=True, blank=True)
+    description     = models.CharField(max_length=255, blank = True)
+    cat_image       = models.ImageField(upload_to='photos/categories/',blank=True)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.category_name)
+        self.slug   = slugify(self.category_name)
         super().save(*args, **kwargs)
 
     def __str__(self):
