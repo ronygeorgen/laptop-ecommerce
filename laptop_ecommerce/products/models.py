fb294a1dfdfa6272c145da931aba7019bd278908
Rony George <ronygeorgengit@gmail.com>
2024-02-13 09:32:07 +0530

    third commit

--- laptop_ecommerce/products/models.py
+++ laptop_ecommerce/products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from category.models import Category
+from django.utils.text import slugify
 
 # Create your models here.
 class MyProducts(models.Model):
@@ -7,4 +9,15 @@ class MyProducts(models.Model):
     description         = models.TextField(max_length=500, blank=True)
     price               = models.IntegerField()
     images              = models.ImageField(upload_to='photos/products')
+    stock               = models.IntegerField()
+    is_available        = models.BooleanField(default=True)
+    category            = models.ForeignKey(Category, on_delete=models.CASCADE)
+    create_date         = models.DateTimeField(auto_add_now=True)
+    modified_date       = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.slug       = slugify(self.product_name)
+        super().save(*args, **kwargs)
     
+    def __str__(self):
+        return self.product_name 
