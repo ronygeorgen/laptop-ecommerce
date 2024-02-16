4717644ef9bc0d00ccef12dace0bca4ee07ccb01
Rony George <ronygeorgengit@gmail.com>
2024-02-16 12:15:44 +0530

    store page done

--- laptop_ecommerce/products/urls.py
+++ laptop_ecommerce/products/urls.py
@@ -4,6 +4,5 @@ from myadmin.views import Dashboard
 
 urlpatterns = [
     path('', ProductView.as_view(), name='product'),
-    path('store/', ProductView.as_view(), {'store': True}, name='store-product-view'),
     path('',Dashboard.as_view(), name='dashboard'),
 ]
