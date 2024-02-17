561a3d3ef478781abc97b2a50d43b52d188967ab
Rony George <ronygeorgeofficial65@gmail.com>
2024-02-17 18:14:20 +0530

    login without otp

--- laptop_ecommerce/accounts/urls.py
+++ laptop_ecommerce/accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('logout/', Home.as_view(), name='home'),
+    path('home/', Home.as_view(), name='home'),
 ]
--- laptop_ecommerce/user/views.py
+++ laptop_ecommerce/user/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import View
 from products.models import MyProducts
+from category.models import Category
 # Create your views here.
 class Home(View):
     def get(self,request):
+    
         products = MyProducts.objects.all().filter(is_available=True)
         context = {
             'products' : products
@@ -12,9 +14,11 @@ class Home(View):
 class Store(View):
     def get(self,request):
         products = MyProducts.objects.all().filter(is_available=True)
+        category_list = Category.objects.all()
         product_count = products.count()
         context = {
             'products' : products,
             'product_count' : product_count,
+            'category_list' : category_list,
         }
         return render(request, 'store/store.html',context)
