95e5fd17f38446515a479e1d0a278877daeb3e95
Rony George <ronygeorgeofficial65@gmail.com>
2024-02-18 21:25:49 +0530

    email sent and user activation success

--- laptop_ecommerce/accounts/views.py
+++ laptop_ecommerce/accounts/views.py
@@ -3,9 +3,20 @@ from django.views import View
 from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages
-# from django.contrib.auth.models import a
-from django.contrib.auth import authenticate,login as auth_login ,logout
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth import authenticate,login as auth_login , logout
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.http import HttpResponse
+
+
+
+#verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 # Create your views here.
 class RegisterView(View):
@@ -20,7 +31,7 @@ class RegisterView(View):
         if form.is_valid():
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
-            phone_number = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
             username = email.split("@")[0]
@@ -29,6 +40,20 @@ class RegisterView(View):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
+
+            #user activation
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html',{
+                'user':user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email =  EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
             messages.success(request,'Registration Successful')
             return redirect ('login')
         context = {
@@ -49,7 +74,6 @@ class LoginView(View):
             mypassword = request.POST.get('password')
             
             user_details = authenticate(email=myemail, password=mypassword)
-            print(user_details)
             
             if user_details:
                 auth_login(request,user_details)
@@ -59,10 +83,27 @@ class LoginView(View):
                 messages.error(request, 'Invalid login credentials')
                 return redirect('login')
         
-        
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class LogoutView(View):
     def get(self,request):
         if request.user.is_authenticated:
             logout(request)
+            messages.success(request, 'You are logged out.')
         return redirect('login')
 
+class ActivateView(View):
+    def get(self, request, uidb64, token):
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = Account._default_manager.get(pk=uid)
+        except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+            user = None 
+        
+        if user is not None and default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+            messages.success(request, 'congratulations, Your account is activated.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Invalid activation link')
+            return redirect ('regiester')
--- laptop_ecommerce/laptop_ecommerce/settings.py
+++ laptop_ecommerce/laptop_ecommerce/settings.py
@@ -144,3 +144,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+#SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'incognitomarian422@gmail.com'
+EMAIL_HOST_PASSWORD = 'vgspdacksmxnmjvr'
+EMAIL_USE_TLS = True
