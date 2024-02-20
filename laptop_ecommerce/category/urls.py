f0ed8b04ec827ffdcf347b08b20a58a25ae367a6
Rony George <ronygeorgengit@gmail.com>
2024-02-20 23:02:34 +0530

    category image displayed

--- laptop_ecommerce/category/urls.py
+++ laptop_ecommerce/category/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . views import *
+from products.views import ProductView
 
 urlpatterns = [
     path('category/',CategoryView.as_view(), name='category'),
+    path('edit_category/<pk>/',EditView.as_view(), name='edit_category'),
 ]
