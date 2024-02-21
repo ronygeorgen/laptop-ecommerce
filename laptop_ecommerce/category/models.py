38959c334ed8911d3028a09faecf90b420b0362b
Rony George <ronygeorgeofficial65@gmail.com>
2024-02-21 10:52:34 +0530

    category CRED operations added

--- laptop_ecommerce/category/models.py
+++ laptop_ecommerce/category/models.py
@@ -3,10 +3,10 @@ from django.utils.text import slugify
 # Create your models here.
 class Category(models.Model):
     category_name   = models.CharField(max_length=50, unique=True)
-    slug            = models.SlugField(max_length=100,unique=True, blank=True)
-    description     = models.CharField(max_length=255, blank = True)
-    cat_image       = models.ImageField(upload_to='static/categories',blank=True)
-
+    slug            = models.SlugField(max_length=100,unique=True)
+    description     = models.CharField(max_length=255)
+    cat_image       = models.ImageField(upload_to='static/categories',blank=True, null=True)
+    is_deleted      = models.BooleanField(default=False)
     def save(self, *args, **kwargs):
         self.slug   = slugify(self.category_name)
         super().save(*args, **kwargs)
--- laptop_ecommerce/category/views.py
+++ laptop_ecommerce/category/views.py
@@ -10,43 +10,80 @@ class CategoryView(View):
 
     def get(self,request):
 
-        list_categories = Category.objects.all()
-        return render(request, self.template, {'categories':list_categories})
+        list_categories_not_deleted = Category.objects.filter(is_deleted=False)
+        list_categories_deleted     = Category.objects.filter(is_deleted=True)
+        context                     = {
+            'categories_not_deleted': list_categories_not_deleted,
+            'categories_deleted'    :  list_categories_deleted,
+        }
+        return render(request, self.template, context)
         
     
     def post(self,request):
         
         form_category        = request.POST.get('product_name')
         form_description     = request.POST.get('product_description')
-        form_category_image  = request.FILES['cat_image']
+        form_category_image  = request.FILES.get('cat_image')
 
-        if form_category and form_description and form_category_image:
-            category = Category(category_name = form_category, description = form_description, cat_image = form_category_image)
+        if form_category and form_description:
+            category = Category(category_name = form_category, description = form_description)
+
+            if form_category_image:
+                category.cat_image = form_category_image
             category.save()
             return redirect('category')
         
         messages.error(request, 'Enter all fields')
-        list_categories = Category.objects.all()
-        entered_data = {
+        list_categories_not_deleted = Category.objects.filter(is_deleted=False)
+        list_categories_deleted     = Category.objects.filter(is_deleted=True)
+        context                     = {
+            'categories_not_deleted': list_categories_not_deleted,
+            'categories_deleted'    :  list_categories_deleted,
             'product_name'           : form_category,
             'product_description'    : form_description
         }
+        # entered_data = {
             
-        return render(request,self.template, {'categories':list_categories, 'entered_data':entered_data})
+        # }
+            
+        return render(request,self.template,context)
     
 
 class EditView(View):
     template = 'admin_templates/evara-backend/page-categories.html'
     def get(self, request, pk):
+        my_id = Category.objects.values('id').filter(pk=pk)
+        print('selected id = ',my_id)
         category_to_edit = Category.objects.get(pk=pk)
-        list_categories = Category.objects.all()
-        return render(request, self.template, {'category': category_to_edit, 'categories':list_categories})
+        list_categories_not_deleted = Category.objects.filter(is_deleted=False)
+        list_categories_deleted     = Category.objects.filter(is_deleted=True)
+        deleted_id = Category.objects.values('id').filter(pk=pk, is_deleted=True)
+        print('deleted id = ',deleted_id)
+        context                     = {
+            'category_to_edit'      : category_to_edit,
+            'categories_not_deleted': list_categories_not_deleted,
+            'categories_deleted'    : list_categories_deleted,
+            'category'              : category_to_edit,
+            'deleted_id'            : deleted_id,
+            'my_id'                 : my_id,
+        }
+        return render(request, self.template, context)
         
     
     def post(self, request, pk):
         category_to_be_edited = Category.objects.get(pk=pk)
         category_to_be_edited.category_name = request.POST.get('product_name')
         category_to_be_edited.description = request.POST.get('product_description')
-        category_to_be_edited.cat_image = request.FILES['cat_image']
+        if 'mycheck' in request.POST:
+            category_to_be_edited.is_deleted    = request.POST.get('mycheck')=='False'
+        if 'cat_image' in request.FILES:
+            category_to_be_edited.cat_image = request.FILES['cat_image']
         category_to_be_edited.save()            
+        return redirect('category')
+    
+class SoftDeleteCategoryView(View):
+    def get(self, request, pk):
+        category = Category.objects.get(pk=pk)
+        category.is_deleted = True
+        category.save()
         return redirect('category')
