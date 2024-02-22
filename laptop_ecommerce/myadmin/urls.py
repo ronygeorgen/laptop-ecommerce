ef20cac16af36264e04acdf9a064850a660be969
Rony George <ronygeorgeofficial65@gmail.com>
2024-02-22 21:18:31 +0530

    Admin side user management done

--- laptop_ecommerce/myadmin/urls.py
+++ laptop_ecommerce/myadmin/urls.py
@@ -3,4 +3,7 @@ from . views import *
 
 urlpatterns = [
     path('dashboard/',Dashboard.as_view(), name='dashboard'),
+    path('user_list/',UserManagementView.as_view(), name='user_list'),
+    path('user_block/<pk>/',UserBlockView.as_view(), name='user_block'),
+    path('user_unblock/<pk>/',UserUnblockView.as_view(), name='user_unblock'),
 ]
--- laptop_ecommerce/myadmin/views.py
+++ laptop_ecommerce/myadmin/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.http import HttpResponse
+from accounts.models import Account
+
 # Create your views here.
 class Dashboard(View):
     def get(self,request):
         return render(request,'admin_templates/evara-backend/index.html')
+class UserManagementView(View):
+    def get(self, request):
+        active_users_list = Account.objects.filter(is_active=True, is_admin=False)
+        blocked_users_list = Account.objects.filter(is_active=False, is_admin=False)
+        context = {
+            'active_users_list': active_users_list,
+            'blocked_users_list': blocked_users_list,
+        }
+        return render (request, 'admin_templates/evara-backend/user-list.html', context)
+
+class UserBlockView(View):
+    def get(self, request, pk):
+        user_to_be_blocked = Account.objects.get(pk=pk)
+        user_to_be_blocked.is_active = False
+        user_to_be_blocked.save()
+        return redirect('user_list')
+        
+class UserUnblockView(View):
+    def get(self, request, pk):
+        user_to_be_unblocked = Account.objects.get(pk=pk)
+        user_to_be_unblocked.is_active = True
+        user_to_be_unblocked.save()
+        return redirect('user_list')
