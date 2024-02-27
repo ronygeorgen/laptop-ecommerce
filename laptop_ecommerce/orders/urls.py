e75a83968357bbf96728111ec97ad78e79f04ea6
Rony George <ronygeorgengit@gmail.com>
2024-02-28 03:47:10 +0530

    Payment PayPal and Order Summary Done

--- laptop_ecommerce/orders/urls.py
+++ laptop_ecommerce/orders/urls.py
@@ -4,4 +4,5 @@ from . views import *
 urlpatterns = [
     path('place_order/',PlaceOrderView.as_view(), name='place_order'),
     path('payments/',PaymentsView.as_view(), name='payments'),
+    path('order_complete/',OrderCompleteView.as_view(), name='order_complete'),
 ]
