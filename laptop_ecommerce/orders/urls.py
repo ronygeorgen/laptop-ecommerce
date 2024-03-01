2f939c42364bb8b59260518fc102b1883ce619b6
Rony George <ronygeorgengit@gmail.com>
2024-03-02 00:22:12 +0530

    payment part done paypal and cod

--- laptop_ecommerce/orders/urls.py
+++ laptop_ecommerce/orders/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('place_order/',PlaceOrderView.as_view(), name='place_order'),
     path('payments/',PaymentsView.as_view(), name='payments'),
     path('order_complete/',OrderCompleteView.as_view(), name='order_complete'),
+    path('cashondelivery/<str:order_number>/',CashOnDeliveryView.as_view(), name='cashondelivery'),
 ]
