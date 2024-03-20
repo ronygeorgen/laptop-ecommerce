684c3eae9fbf5772f122a285edc239ec6cafe396
Rony George <ronygeorgengit@gmail.com>
2024-03-20 16:17:07 +0530

    Admin side aside bar cleaned

--- laptop_ecommerce/coupon_Mng/forms.py
+++ laptop_ecommerce/coupon_Mng/forms.py
@@ -10,3 +10,13 @@ class CouponForm(forms.ModelForm):
             'end_date': forms.DateInput(attrs={'class':'form-control', 'type':'date'}),
             'discount_rate': forms.NumberInput(attrs={'class':'form-control'}),
         }
+    def clean(self):
+        cleaned_data = super().clean()
+        coupon_id = cleaned_data.get('coupon_id')
+        end_date = cleaned_data.get('end_date')
+        discount_rate = cleaned_data.get('discount_rate')
+
+        if not coupon_id or not end_date or not discount_rate:
+            raise forms.ValidationError("All fields are required.")
+
+        return cleaned_data
