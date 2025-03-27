b169e6802077aa56f9340ee320b273a87a6a796c
Rony George <ronygeorgengit@gmail.com>
2025-03-27 16:36:39 +0530

    update settings

--- laptop_ecommerce/laptop_ecommerce/settings.py
+++ laptop_ecommerce/laptop_ecommerce/settings.py
@@ -135,12 +135,13 @@ USE_TZ = True
 
 import os
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+# STATIC_ROOT = BASE_DIR / "static"
 # STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-STATICFILES_DIRS = [
-    "laptop_ecommerce/static",
-]
+# STATICFILES_DIRS = [
+#     "laptop_ecommerce/static",
+# ]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile/')
 
